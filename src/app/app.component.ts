0b36460ba518328fe84ed61f577258a050e3f88e
TheHerrDani <Sipos.Herr.Dani@gmail.com>
2019-02-05 18:48:57 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient){}
   title = 'PrisonSchool-UI';
+
+  ngOnInit(){
+    console.log('prisonschool is running');
+    this.http.get('localhost:8080/PrisonSchool/Main').subscribe(val => console.log(val));
+  }
+
 }
